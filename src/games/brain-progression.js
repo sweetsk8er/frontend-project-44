220041fd27ada6ea2596a6c9f6381d827bebca72
sweetsk8er <cranefront@yandex.ru>
2023-01-11 16:07:36 +0300

    фикс и правки от куратора

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -3,10 +3,7 @@ import playGame from '../index.js';
 
 const discriptionProg = 'What number is missing in the progression?';
 
-const getProgress = () => {
-  const start = getRandom(1, 100);
-  const step = getRandom(1, 10);
-  const length = getRandom(5, 10);
+const getProgress = (start, step, length) => {
   const progression = [];
 
   for (let i = 0; i < length; i += 1) {
@@ -16,7 +13,10 @@ const getProgress = () => {
 };
 
 const genRound = () => {
-  const progression = getProgress();
+  const start = getRandom(1, 100);
+  const step = getRandom(1, 10);
+  const length = getRandom(5, 10);
+  const progression = getProgress(start, step, length);
   const randomIndex = getRandom(0, progression.length - 1);
   const randomPartInProgression = progression[randomIndex];
   const answer = String(randomPartInProgression);
